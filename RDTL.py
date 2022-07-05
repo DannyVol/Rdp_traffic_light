00f41a1deaedebda22178a09a4785297fc433a1a
danielvo <dannyvolk69@gmail.com>
2022-07-05 19:56:25 +0300

    Added: multi threading module, RDP connaction handlers, DNS query

--- RDTL.py
+++ RDTL.py
@@ -1,11 +1,37 @@
 import time
-
 import Sys
+import socket
+import threading
 
+### global state ###
+hostName = socket.gethostname()
+print(hostName)
+inHostAddr = Sys.getHostAddr()
+print(inHostAddr)
+print(Sys.getOutHostAddr())
 
-while True:
-    if (Sys.getActivePort(3389) == True):
-        print("true")
-    else:
-        Sys.consolUser()
-    time.sleep(2)
+### Multi threading starter ###
+def sTarter(targetX):
+    threading.Thread(target=targetX).start()
+
+def mainLogic():
+    while True:
+        if (Sys.getActivePort(3389) == True):
+            rHost = Sys.getRhost(Sys.getRhostADDR())
+            print('True')
+        else:
+            Sys.consolUser()
+        time.sleep(2)
+
+def outConn():    
+    while True:
+        d = Sys.getOutHostAddr()
+        if (d == 'None'):
+            print("outbound connection")
+        else:
+            print('no outbound connection')
+        time.sleep(2)
+
+sTarter(mainLogic)
+sTarter(outConn)
+
--- Sys.py
+++ Sys.py
@@ -1,13 +1,13 @@
 import psutil
 import time
+import socket
 
+### No RDP inbound connaction logic ###
 def consolUser():
     x = psutil.users()
     for i in x:
         if (i.host == 'localhost' or 'none'):
             return print(i.name)
-
-
 
 
 #### Find if getPort is in ESTABLISHED status ####
@@ -21,3 +21,35 @@ def getActivePort(getPort):
         except AttributeError:
             pass
     return False
+
+### Find remote host ###
+def getRhostADDR():
+    try:
+        d = psutil.net_connections()
+        rHostaddr = [x.raddr.ip for x in d if x.status == 'ESTABLISHED' and x.laddr.port == 3389]
+        return rHostaddr
+    except IndexError:
+        return None
+
+### Find RDP inbound connaction if_adddres ###
+def getHostAddr():
+    try:
+        d = psutil.net_connections()
+        hostAddr = [x.laddr.ip for x in d if x.status == 'ESTABLISHED' and x.laddr.port == 3389]
+        return hostAddr[0]
+    except IndexError:
+        return None
+
+### Find RDP outbound connaction if_adddres ###
+def getOutHostAddr():
+    try:
+        d = psutil.net_connections()
+        hostAddr = [x.laddr.ip for x in d if x.status == 'ESTABLISHED' and x.raddr.port == 3389]
+        return hostAddr[0]
+    except IndexError:
+        return None
+    
+### DNS query to get hostname by ip ###
+def getRhost(addr):
+    hostName = socket.gethostbyaddr(addr)
+    return hostName[0]
